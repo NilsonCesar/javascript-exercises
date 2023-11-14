74029488cb6eefec48bf41d6dd3bd0db31289e72
Nilson Cesar <admncoaf@gmail.com>
2023-11-14 12:40:12 -0300

    Solve exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (typeof a == "number" && typeof b == "number" && a >= 0 && b >= 0) {
+        return parseInt((Math.abs(b - a) + 1) * (a + b)/ 2);
+    }
+    return "ERROR";
 };
-
+sumAll(123, 1);
 // Do not edit below this line
 module.exports = sumAll;
