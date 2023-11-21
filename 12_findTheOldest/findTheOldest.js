94438fdabe1db9c90986916213b561a00ec073c8
Nilson Cesar <admncoaf@gmail.com>
2023-11-21 15:58:43 -0300

    Solve exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let ages = arr.map(people => people.yearOfDeath - people.yearOfBirth || 2023 - people.yearOfBirth);
+    let oldestAge = -1;
+    let oldest = '';
 
+    for(let i = 0; i < ages.length; i++) {
+        if(oldestAge < ages[i]) {
+            oldestAge = ages[i];
+            oldest = arr[i];
+        }
+    }
+
+    return oldest;
 };
 
 // Do not edit below this line
