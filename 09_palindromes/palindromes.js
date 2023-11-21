89efed707dcb9ca60c47c95d9a2196b16ce979a0
Nilson Cesar <admncoaf@gmail.com>
2023-11-21 15:30:49 -0300

    Solve exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    strFormated = (Array.from(str)).filter(c => ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') || ('1' <= c && c <= '9'));
+    strFormated = (strFormated.reduce((str, a) => str + a, ''));
+    strFormated = strFormated.toLowerCase();
 
+    for(let i = 0; i < strFormated.length; i++) {
+        if(strFormated[i] != strFormated[strFormated.length - i -1]) {
+            return false;
+        }
+    }
+
+    return true;
 };
 
 // Do not edit below this line
